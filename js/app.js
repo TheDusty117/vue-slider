d24b9fd7c0ab9900a3b55e971ee78ed6de7e66be
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-20 18:20:01 +0100

    FEAT correct logic working with vue

--- js/app.js
+++ js/app.js
@@ -3,46 +3,58 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        //array di oggetti
+        //ARRAY DI OGGETTI
         slideArrObjects : [
           {
-            isActive: 'false',
             sourceImg: './img/image1.0.jpg',
             title: 'Fiume Vietnamita',
             description: 'Una bellissima alba sul fiume Mekong',
           },
           {
-            isActive: 'false',
             sourceImg: './img/image2.0.jpg',
             title: 'Spiaggia Rocce Noosa',
             description: 'Il tramonto Australiano',
           },
           {
-            isActive: 'false',
             sourceImg: './img/image3.0.jpg',
             title: 'Montagne Canadesi',
             description: 'La glaciale solitudine della montagna',
           },
           {
-            isActive: 'false',
             sourceImg: './img/image4.0.jpg',
             title: 'Tramonto sulle Ande',
             description: 'Il sole tramonta sulle impervie montagne',
           },
           {
-            isActive: 'false',
             sourceImg: './img/image5.0.jpg',
             title: 'Notte nella Savana',
             description: 'Gli astri danzano nella notte Africana',
           }
         ],
+
+        currentIndex: 0,
+
+        lastIndex: 5,
+
       }
     },
+
+
+    //METODI FUNZIONI
     methods:{
-      classIsActive(){
-        console.log('this.isActive', this.isActive)
-        
-      }
+
+      nextIndex(){
+        if (this.currentIndex < 4){
+          this.currentIndex++
+        }
+      },
+
+      prevIndex(){
+        if (this.currentIndex > 0){
+          this.currentIndex--
+        }
+      },
+
     }
   }).mount('#carousel')
 
@@ -59,77 +71,70 @@ const { createApp } = Vue
 
 
 
-  const slideElements = document.getElementsByClassName('slide') //prendo tutti gli elementi con classe slide.
+  // const slideElements = document.getElementsByClassName('slide') //prendo tutti gli elementi con classe slide.
 
+  // //VERSIONE ON CLICK DELLO SCORRIMENTO DELLE SLIDE
+  
+  // slideElements[0].classList.add('active')
   
   
+  // const leftBtnElement = document.getElementById('arrow-left')
+  
+  // const rightBtnElement = document.getElementById('arrow-right')
+  
+  // console.log(slideElements)
+  // console.log(leftBtnElement, rightBtnElement)
+  
+  // let currentIndex = 0
+  // // FUNZIONALITA' AL CLICK DEL BOTTONE DESTRO (PER ANDARE AVANTI)
+  
+  // rightBtnElement.addEventListener('click', function() {
+  //   console.log('slide next', currentIndex)
+  // //USO CONDIZIONE IF ovvero appena arriva al click 6 lei smettera' di andare avanti evitando di dare errore
+  
+  //     //CONDIZIONE IF lunghezza dell'array -1 in modo tale da non dare erroe se vado oltre la sua LUNGHEZZA
+  //   slideNext();
+  
+  // })
   
   
-  
-  
-  
-  //VERSIONE ON CLICK DELLO SCORRIMENTO DELLE SLIDE
-  
-  slideElements[0].classList.add('active')
-  
-  
-  const leftBtnElement = document.getElementById('arrow-left')
-  
-  const rightBtnElement = document.getElementById('arrow-right')
-  
-  console.log(slideElements)
-  console.log(leftBtnElement, rightBtnElement)
-  
-  let currentIndex = 0
-  // FUNZIONALITA' AL CLICK DEL BOTTONE DESTRO (PER ANDARE AVANTI)
-  
-  rightBtnElement.addEventListener('click', function() {
-    console.log('slide next', currentIndex)
-  //USO CONDIZIONE IF ovvero appena arriva al click 6 lei smettera' di andare avanti evitando di dare errore
-  
-      //CONDIZIONE IF lunghezza dell'array -1 in modo tale da non dare erroe se vado oltre la sua LUNGHEZZA
-    slideNext();
-  
-  })
-  
-  
-  leftBtnElement.addEventListener('click', function() { //la slide attiva deve essere >= a 1 della slide attiva
-    console.log('slide back', currentIndex)
+  // leftBtnElement.addEventListener('click', function() { //la slide attiva deve essere >= a 1 della slide attiva
+  //   console.log('slide back', currentIndex)
     
-    slideBack();
+  //   slideBack();
   
-  })
-  
-  
+  // })
   
   
   
   
-  function slideBack() {
-    if (currentIndex > 0) {
   
-      let currentSlide = slideElements[currentIndex];
-      currentSlide.classList.remove('active');
   
-      currentIndex -= 1;
+  // function slideBack() {
+  //   if (currentIndex > 0) {
   
-      let nextSlide = slideElements[currentIndex];
-      nextSlide.classList.add('active');
-    }
-  }
+  //     let currentSlide = slideElements[currentIndex];
+  //     currentSlide.classList.remove('active');
   
-  function slideNext() {
-    if (currentIndex < slideElements.length - 1) {
+  //     currentIndex -= 1;
   
-      //nascondere la slide attiva togliendo la classe 'active'
-      let currentSlide = slideElements[currentIndex];
-      currentSlide.classList.remove('active');
+  //     let nextSlide = slideElements[currentIndex];
+  //     nextSlide.classList.add('active');
+  //   }
+  // }
   
-      //incrementare l'indice
-      currentIndex += 1;
+  // function slideNext() {
+  //   if (currentIndex < slideElements.length - 1) {
   
-      //spostare classe 'active' e mostrare la slide successiva
-      let nextSlide = slideElements[currentIndex];
-      nextSlide.classList.add('active');
-    }
-  }
+  //     //nascondere la slide attiva togliendo la classe 'active'
+  //     let currentSlide = slideElements[currentIndex];
+  //     currentSlide.classList.remove('active');
+  
+  //     //incrementare l'indice
+  //     currentIndex += 1;
+  
+  //     //spostare classe 'active' e mostrare la slide successiva
+  //     let nextSlide = slideElements[currentIndex];
+  //     nextSlide.classList.add('active');
+  //   }
+  // }
